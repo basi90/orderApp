2d27ddf866d9111bbb7075da260dad6be7bbd1d7
Andrea Basili <andrea.basili@live.it>
2025-04-25 15:05:33 +0200

    move code to proper UserController class, test on swagger good

--- src/test/java/com/ab/order/webapi/UserControllerTest.java
+++ src/test/java/com/ab/order/webapi/UserControllerTest.java
@@ -1,24 +1,4 @@
 package com.ab.order.webapi;
 
-import com.ab.order.service.DTOs.UserInputDTO;
-import com.ab.order.service.DTOs.UserOutputDTO;
-import com.ab.order.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/users")
 public class UserControllerTest {
-
-    private final UserService userService;
-
-    public UserControllerTest(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserOutputDTO registerUser(@RequestBody UserInputDTO input) {
-        return userService.createUser(input);
-    }
 }
